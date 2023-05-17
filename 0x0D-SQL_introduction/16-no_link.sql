227007eff1df11f2a9b19c4c66b8e9a241df86a1
EnobongEdo <onlynenny@gmail.com>
2023-05-16 19:05:37 -0700

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -5,7 +5,4 @@
 -- Records should be listed by descending score
 -- The database name will be passed as an argument to the mysql command
 
-SELECT `score`, COUNT(*) AS `number`
-FROM `second_table`
-GROUP BY `score`
-ORDER BY `number` DESC; 
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
