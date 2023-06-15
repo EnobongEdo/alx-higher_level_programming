c555e2898af9f85312f4538af9f222b0ffb2c66a
EnobongEdo <onlynenny@gmail.com>
2023-06-14 21:07:30 -0700

    Task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// This is a module defines a Rectangle class
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
