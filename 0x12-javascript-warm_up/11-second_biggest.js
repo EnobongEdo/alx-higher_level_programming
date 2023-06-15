5839635804a3dd72ce00cfdcbd6ac9bf89084d58
EnobongEdo <onlynenny@gmail.com>
2023-06-14 20:38:46 -0700

    Task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+//  This is a script that searches the second biggest integer in the list of arguments.
+//
+//  Assume all arguments can be converted to integer
+//  If no argument passed, print 0
+//  If the number of arguments is 1, print 0
+//  Use console.log(...) to print all output
+//  You are not allowed to use var
+
+const args = process.argv.slice(2);
+if (args.length === 0 || args.length === 1) {
+  console.log(0);
+} else {
+  const sort = args.map((ele) => Number(ele)).sort((a, b) => (b - a));
+  console.log(sort[1]);
+}
