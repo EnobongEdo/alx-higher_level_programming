eb6a80b9cd46213dd1772980f208dd54343e2508
EnobongEdo <onlynenny@gmail.com>
2023-07-01 22:35:17 -0700

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
+"""
+Prints peak value
+"""
 
+
+def find_peak(list_of_integers):
+    """
+    Returns peak
+    """
+    if len(list_of_integers) == 0:
+        return None
+    long = len(list_of_integers) - 1
+    small = 0
+    while small < long:
+        mid = (long + small) // 2
+        if list_of_integers[mid] < list_of_integers[mid+1]:
+            small = mid + 1
+        else:
+            long = mid
+    return list_of_integers[small]
